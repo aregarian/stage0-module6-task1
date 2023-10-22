1a42d612fa48ee8594f3e48e185f36b143312e7f
Fanny Dorthey <fannydor@ev-k6.kzn.21-school.ru>
2023-10-22 12:53:43 +0300

    awerawerwerqgyujkgkrq

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -5,7 +5,7 @@ public class Animal {
         private int numberOfPaws;
         private boolean hasFur;
 
-        public Animal(String color, int numberOfPaws, boolean hasFur) {
+        public Animal(String color, int numberOfPaws, boolean hasFur){
             this.color = color;
             this.numberOfPaws = numberOfPaws;
             this.hasFur = hasFur;
